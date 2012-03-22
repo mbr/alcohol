194f74b6d353211904f3fc698b3e81deb983d722
Marc Brinkmann <git@marcbrinkmann.de>
2012-03-22 18:02:30 +0100

    Added inline docs to tokengen.

--- alcohol/tokengen.py
+++ alcohol/tokengen.py
@@ -24,6 +24,20 @@ class InvalidTokenException(TokenException):
 
 
 class TokenGenerator(object):
+    """Generates tokens using pbkdf2 and a secret key. Tokens can be given
+       an expiration date after which they are no longer valid and be tied to a
+       secret key.
+
+       :param secret_key: The secret key used by the application. Used to
+                          ensure that generated tokens stem from this
+                          application.
+       :param pbkdf_keylength: The length of the key to generate for use in the
+                          token. This directly affects token length.
+       :param pbkdf_iterations: The number of iterations that pbkdf runs.
+       :param pbkdf_saltlength: The length of the salt, taken from
+                          :py:func:`os.urandom`.
+       :param pbkdf_hashfunc: The hash function used.
+       """
     max_expires = 2 ** 63 - 1
     _pack_format = '!q'
     _expires_token_length = len(hexlify(pack(_pack_format, max_expires)))
@@ -56,6 +70,16 @@ class TokenGenerator(object):
                           self.hashfunc)
 
     def generate_token(self, expires=-1, bound_value=None):
+        """Generates a new token.
+
+        :param expires: A unix timestamp of when the token should be considered
+                        expired. Must be an integer and fit into 8 bytes.
+        :param bound_value: A value tied to this token. This basically acts as
+                            a second token-specific secret key.
+        :return: A hex string containing salt, expiry date and key combined
+                into one. Its length will be
+                :py:attr:`token_length` digits.
+        """
         assert(self.max_expires >= expires)
         salt = hexlify(os.urandom(self.pbkdf_saltlength))
         expires_packed = hexlify(pack(self._pack_format, expires))
@@ -64,6 +88,10 @@ class TokenGenerator(object):
 
     @property
     def token_length(self):
+        """The token length.
+
+        Contains the exact of generated tokens, in bytes. Note that tokens are
+        returned as hex strings. This is the length of the returned strings."""
         return self._salt_token_length +\
                self._expires_token_length +\
                self._key_token_length
@@ -89,6 +117,15 @@ class TokenGenerator(object):
         return salt, expires, key
 
     def get_expiry_time(self, token, bound_value=None):
+        """Extracts the expiration time from a token.
+
+        :param token: The token to be examined.
+        :param bound_value: The same bound value that was used to generate the
+                            token.
+        :return: The expiration time as an integer.
+        :throw TokenException: If the token is not valid other than its
+                               expiration.
+        """
         salt, expires, key = self._unpack_token(token)
 
         real_key = self._generate_key(salt, expires, bound_value)
@@ -97,7 +134,16 @@ class TokenGenerator(object):
 
         return expires
 
-    def token_expired(self, token, bound_value=None):
+    def token_not_expired(self, token, bound_value=None):
+        """Checks if a token is expired.
+
+        :param token: The token to be examined.
+        :param bound_value: The same bound value that was used to generate the
+                            token.
+        :return: True if the token is NOT expired, False if it.
+        :throw TokenException: If the token is not valid other than its
+                               expiration.
+        """
         expiry_time = self.get_expiry_time(token, bound_value)
 
         # a token with an expiry time of -1 does not expire
@@ -110,7 +156,17 @@ class TokenGenerator(object):
         return True
 
     def check_token(self, token, bound_value=None):
+        """Check a token for full validity.
+
+        This is the function you should be using to verify tokens, most of the
+        time.
+
+        :param token: The token to be examined.
+        :param bound_value: The same bound value that was used to generate the
+                            token.
+        :return: True if the token is valid, False if it is NOT.
+        """
         try:
-            return self.token_expired(token, bound_value)
+            return self.token_not_expired(token, bound_value)
         except TokenException:
             return False
