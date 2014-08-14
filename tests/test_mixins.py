0d35465885f78ee6f9b6ee8ddbe872ca411469a3
Marc Brinkmann <git@marcbrinkmann.de>
2014-08-14 16:17:58 +0200

    Test SQLAlchemy mixins as well.;

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -3,8 +3,11 @@
 
 import time
 
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
 from alcohol.mixins import *
 from alcohol.mixins.sqlalchemy import *
+from pytest_extra import group_fixture
 from passlib.context import CryptContext
 import pytest
 from six import b, int2byte, indexbytes
@@ -39,20 +42,30 @@ class KwargsBaseType(object):
 
 
 @pytest.fixture
-def user_type_pw(passlib_ctx):
-    class User(KwargsBaseType, PasswordMixin):
-        pass
+def user_type_vanilla(passlib_ctx):
+    class VanillaUserPw(KwargsBaseType, PasswordMixin, EmailMixin):
+        email = None
 
-    User.crypt_context = passlib_ctx
-    return User
+    VanillaUserPw.crypt_context = passlib_ctx
+
+    return VanillaUserPw
 
 
 @pytest.fixture
-def user_type_ml():
-    class User(EmailMixin):
-        email = None
+def user_type_sqlalchemy(passlib_ctx):
+    Base = declarative_base()
 
-    return User
+    class SQLAlchemyUser(Base, SQLAlchemyEmailMixin, SQLAlchemyPasswordMixin):
+        __tablename__ = 'users'
+        id = Column(Integer(), primary_key=True)
+        crypt_context = passlib_ctx
+
+    return SQLAlchemyUser
+
+
+@group_fixture
+def user_type():
+    return ['user_type_vanilla', 'user_type_sqlalchemy']
 
 
 @pytest.fixture
@@ -74,47 +87,47 @@ def tamper_with(bs):
         yield bs[:i] + new_byte + bs[i + 1:]
 
 
-def test_password_check(user_type_pw):
+def test_password_check(user_type):
     valid_password = 's0m3p4$$w0rd'
     invalid_password = valid_password + 'x'
-    user = user_type_pw(password=valid_password)
+    user = user_type(password=valid_password)
 
     assert user.check_password(valid_password)
     assert not user.check_password(invalid_password)
 
 
-def test_salted_passwords_are_not_equal(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
-    user2 = user_type_pw(password=pw)
+def test_salted_passwords_are_not_equal(user_type, pw, secret_key):
+    user = user_type(password=pw)
+    user2 = user_type(password=pw)
 
     assert user._pwhash != user2._pwhash
 
 
-def test_password_reset_tokens_are_different(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_password_reset_tokens_are_different(user_type, pw, secret_key):
+    user = user_type(password=pw)
     token1 = user.create_reset_password_token(secret_key)
     token2 = user.create_reset_password_token(secret_key)
 
     assert token1 != token2
 
 
-def test_password_reset_token_works(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_password_reset_token_works(user_type, pw, secret_key):
+    user = user_type(password=pw)
     token = user.create_reset_password_token(secret_key)
 
     assert user.check_password_reset_token(secret_key, token)
 
 
-def test_password_reset_is_tamperproof(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_password_reset_is_tamperproof(user_type, pw, secret_key):
+    user = user_type(password=pw)
     token = user.create_reset_password_token(secret_key)
 
     for bad_token in tamper_with(token):
         assert not user.check_password_reset_token(secret_key, bad_token)
 
 
-def test_password_reset_expires(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_password_reset_expires(user_type, pw, secret_key):
+    user = user_type(password=pw)
 
     token = user.create_reset_password_token(secret_key)
     time.sleep(2)  # resolution in itsdangerous is 1 second
@@ -124,8 +137,8 @@ def test_password_reset_expires(user_type_pw, pw, secret_key):
     )
 
 
-def test_two_tokens_work_both(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_two_tokens_work_both(user_type, pw, secret_key):
+    user = user_type(password=pw)
     token1 = user.create_reset_password_token(secret_key)
     token2 = user.create_reset_password_token(secret_key)
 
@@ -134,8 +147,8 @@ def test_two_tokens_work_both(user_type_pw, pw, secret_key):
     assert user.check_password_reset_token(secret_key, token2)
 
 
-def test_tokens_stops_working_after_pw_change(user_type_pw, pw, secret_key):
-    user = user_type_pw(password=pw)
+def test_tokens_stops_working_after_pw_change(user_type, pw, secret_key):
+    user = user_type(password=pw)
     token1 = user.create_reset_password_token(secret_key)
     token2 = user.create_reset_password_token(secret_key)
 
@@ -145,15 +158,15 @@ def test_tokens_stops_working_after_pw_change(user_type_pw, pw, secret_key):
     assert not user.check_password_reset_token(secret_key, token1)
 
 
-def test_password_cannot_be_read(user_type_pw, pw, secret_key):
-    user = user_type_pw()
+def test_password_cannot_be_read(user_type, pw, secret_key):
+    user = user_type()
 
     with pytest.raises(TypeError):
         user.password
 
 
-def test_email_verification_token_works(user_type_ml, secret_key, email):
-    user = user_type_ml()
+def test_email_verification_token_works(user_type, secret_key, email):
+    user = user_type()
 
     token = user.create_email_activation_token(secret_key, email)
     assert user.activate_email(secret_key, token)
@@ -161,9 +174,9 @@ def test_email_verification_token_works(user_type_ml, secret_key, email):
 
 
 def test_email_verification_token_altered_fails(
-    user_type_ml, secret_key, email
+    user_type, secret_key, email
 ):
-    user = user_type_ml()
+    user = user_type()
 
     token = user.create_email_activation_token(secret_key, email)
 
@@ -172,16 +185,16 @@ def test_email_verification_token_altered_fails(
         assert user.email is None
 
 
-def test_email_verification_token_usable_once(user_type_ml, secret_key, email):
-    user = user_type_ml()
+def test_email_verification_token_usable_once(user_type, secret_key, email):
+    user = user_type()
 
     token = user.create_email_activation_token(secret_key, email)
     assert user.activate_email(secret_key, token)
     assert not user.activate_email(secret_key, token)
 
 
-def test_can_use_any_email_token(user_type_ml, secret_key, email):
-    user = user_type_ml()
+def test_can_use_any_email_token(user_type, secret_key, email):
+    user = user_type()
 
     # create some meaningless tokens
     user.create_email_activation_token(secret_key, email)
